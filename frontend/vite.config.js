dc07744b46a830cacb1ba8cade275f4bae677c42
mohamaddousti18 <mohamaddousti18@gmail.com>
2025-04-16 21:50:38 +0000

    Fix: Update esbuild settings in vite.config.js to resolve JSX parsing error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,
-    },
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
   },
-};
+})
